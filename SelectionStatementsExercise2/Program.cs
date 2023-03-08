223fbb295995af8a7600c96772f554c42aa30c2c
Jake Johnston <jakejohnston618@gmail.com>
2023-03-08 14:42:36 -0600

    created conditions

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, what's your favorite school subject");
+            var answer = Console.ReadLine();
+            
+            switch (answer)
+            {
+                case "math":
+                    {
+                        Console.WriteLine("Math was my favorite too!");
+                        break;
+                    }
+                case "science":
+                    {
+                        Console.WriteLine("Science was a lot of fun in school!");
+                        break;
+                    }
+                case "history":
+                    {
+                        Console.WriteLine("History is always incredibly interesting!");
+                        break;
+                    }
+                case "english":
+                    {
+                        Console.WriteLine("seriously? that's your favorite?");
+                        break;
+                    }
+                case "athletics":
+                    {
+                        Console.WriteLine("Ohhhh, you're one of those kids. Just kidding, I liked that too!");
+                        break;
+                    }
+                case "default":
+                    {
+                        Console.WriteLine("I don't know what that class is.");
+                        break;
+                    }
+
+                    
+            }
+                   
         }
     }
 }
